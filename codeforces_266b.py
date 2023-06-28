6976dcd82b6ae98241650080f8e292906529b474
Jaspers W. Huanay <hwilliamnq@gmail.com>
2023-06-28 13:30:49 +0300

    accepted

--- codeforces_266b.py
+++ codeforces_266b.py
@@ -8,22 +8,22 @@
 n, t = map(int, input().split())
 queue = input()
 
-stack = []
 
-i = 0
-while i < n:
-    if queue[i] == 'B':
-        j = 0
-        while j < t and i + j < n -1:
-            if queue[i + j + 1] == 'G':
-                stack.append('G')
-                j += 1
-            else: break
-        stack.append('B')
-        i = i + j + 1
+j = 0
+while j < t:
+    i = 0
+    stack = []
+    while i < n:
+        if queue[i] == 'B' and i < n - 1 and queue[i + 1] == 'G':
+            stack.append('G')
+            stack.append('B')
+            i = i + 2
 
-    else: 
-        stack.append('G')
-        i += 1
+        else: 
+            stack.append(queue[i])
+            i += 1
+
+    queue = ''.join(stack)
+    j += 1
 
 print(''.join(stack))
