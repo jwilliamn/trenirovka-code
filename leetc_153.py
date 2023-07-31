ee5bd113d48213de497366ed277c45cd686f6ea1
Jaspers W. Huanay <hwilliamnq@gmail.com>
2023-07-31 14:10:30 +0300

    lc 153 faster

--- leetc_153.py
+++ leetc_153.py
@@ -4,6 +4,36 @@
 # Leet Code problem: 153
 
 
+# Slow
+# class Solution:
+#     def findMin(self, nums: List[int]) -> int:
+#         low = 0
+#         high = len(nums) - 1
+
+#         min_val = min(nums[low], nums[high])
+#         while low <= high:
+
+#             mid = (low + high)//2
+#             #print(low, high)
+#             #print(min_val)
+#             if min_val == nums[low]:
+#                 return min_val 
+#             elif nums[low] > min_val:
+#                 if nums[mid] > min_val:
+#                     low = mid + 1
+#                 else: 
+#                     min_val = nums[mid]
+#                     high = mid - 1
+#             else:
+#                 if nums[mid] > min_val:
+#                     low = mid + 1
+#                 else: 
+#                     min_val = nums[mid]
+#                     high = mid - 1
+#             #print(min_val)
+#         return min_val
+
+# Faster
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         low = 0
@@ -11,24 +41,16 @@ class Solution:
 
         min_val = min(nums[low], nums[high])
         while low <= high:
-
+            if nums[low] < nums[high]:
+                return min(min_val, nums[low])
+            
             mid = (low + high)//2
-            #print(low, high)
-            #print(min_val)
-            if min_val == nums[low]:
-                return min_val 
-            elif nums[low] > min_val:
-                if nums[mid] > min_val:
-                    low = mid + 1
-                else: 
-                    min_val = nums[mid]
-                    high = mid - 1
+            min_val = min(min_val, nums[mid])
+            if nums[mid] >= nums[low]:
+                low = mid + 1
             else:
-                if nums[mid] > min_val:
-                    low = mid + 1
-                else: 
-                    min_val = nums[mid]
-                    high = mid - 1
-            #print(min_val)
+                high = mid - 1
+
         return min_val
+
     
