3ed65473708d46d3b4b458c7d13e21ebbda97b3c
Jaspers W. Huanay <hwilliamnq@gmail.com>
2023-08-18 12:57:46 +0300

    leetcode 206

---
+++ leetc_206.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Leet Code problem: 206
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# Iteratively
+# class Solution:
+#     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         prev = None
+#         curr = head
+
+#         while curr:
+#             next_p = curr.next
+#             curr.next = prev
+#             prev = curr
+#             curr = next_p
+
+#         return prev
+
+# Recursively
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        if head == None or head.next == None:
+            return head
+
+        new_head = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+
+        return new_head
+            
