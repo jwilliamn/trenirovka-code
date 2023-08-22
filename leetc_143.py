7317310c244be063a95ef0c1a3b6ce97cd7b6b9b
Jaspers W. Huanay <hwilliamnq@gmail.com>
2023-08-22 13:21:41 +0300

    leetc 143

---
+++ leetc_143.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Leet Code problem: 143
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+
+        # find the second half
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # traverse the second half
+        second = slow.next
+        prev = slow.next = None
+        while second:
+            nextp = second.next
+            second.next = prev
+            prev = second
+            second = nextp
+
+        # merge the second and first half
+        first, second = head, prev
+        while second:
+            t1, t2 = first.next, second.next
+            first.next = second
+            second.next = t1
+            first, second = t1, t2
